93b3265b39305f5cfa866afc0d56e0dbdfc38c01
lljie <lilingjie1992@gmail.com>
2020-09-08 11:16:46 +0800

    Updates

---
+++ precache-manifest.09b50d2ed8fbfd630fa6393b5f0b45e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a47de2846dbf5214a09b3aee9111fc",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "8d834885e2e4334abcaf",
+    "url": "/react-project/static/css/2.56d49a97.chunk.css"
+  },
+  {
+    "revision": "4d9b2c56c4759887cf3b",
+    "url": "/react-project/static/css/main.29edf343.chunk.css"
+  },
+  {
+    "revision": "8d834885e2e4334abcaf",
+    "url": "/react-project/static/js/2.8c0610f9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-project/static/js/2.8c0610f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d9b2c56c4759887cf3b",
+    "url": "/react-project/static/js/main.82e42272.chunk.js"
+  },
+  {
+    "revision": "1bfa133aed7528cf7e54",
+    "url": "/react-project/static/js/runtime-main.b03b4544.js"
+  }
+]);
